54941de678c7e673d6919be484c05bee587d1994
Odukwe Precious <odukwechiemeka@gmail.com>
2022-07-22 23:33:49 +0100

    almost there

--- Script/dist/main.js
+++ Script/dist/main.js
@@ -1,6 +1,10 @@
 "use strict";
 let closeButton = document.querySelector(".close-button");
+let openButton = document.getElementById("nav-menu");
 let navList = document.querySelector(".nav-list");
 closeButton === null || closeButton === void 0 ? void 0 : closeButton.addEventListener("click", function () {
     navList.style.width = "0";
+});
+openButton === null || openButton === void 0 ? void 0 : openButton.addEventListener("click", function () {
+    navList.style.width = "100%";
 });
--- Script/main.ts
+++ Script/main.ts
@@ -1,7 +1,13 @@
 let closeButton = document.querySelector(".close-button");
 
+let openButton = document.getElementById("nav-menu");
+
 let navList = document.querySelector(".nav-list") as HTMLElement;
 
 closeButton?.addEventListener( "click", function() {
   navList.style.width = "0";
+} );
+
+openButton?.addEventListener( "click", function() {
+  navList.style.width = "100%";
 } );
